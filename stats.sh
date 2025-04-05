d008059e9cbf7fced7ef5c237740b7ad2dc2b63a
Michael Cozier <mikecozier@hotmail.com>
2025-04-05 16:33:43 -0400

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -11,7 +11,7 @@ DATE=$(date | awk '{print $1 " " $2 $3 " " $7 "," $4 $5}')
 INTERNET_UP=$(ping -c 4 8.8.8.8 > /dev/null 2>&1 && echo "Available." || echo "No internet connection.")
 
 # Interface Connections
-WIFI=$(ip link show | grep "enp3s0" | awk '{print $2 $8":" $9}')
+ETHERNET=$(ip link show | grep "enp3s0" | awk '{print $2 $8":" $9}')
 DOCKER=$(ip link show | grep "docker0" | awk '{print $2 $8 ":" $9}' | grep "state:UP")
 
 # Bandwidth
@@ -50,7 +50,7 @@ INODE=$(df -i | awk 'NR>1 && $5+0 > 10 {print "Filesystem: "$1"\nInode Usage: "$
 #Users logged in
 USERS=$(who | awk '{print$1 $5}')
 
-#Failed Login
+#Failed Login Loop
 FAILED_LOGINS=$(journalctl -u ssh --since "5 days ago" | grep "Failed" | while read -r line; do
     # Extract username, IP, and any other useful info
     USERNAME=$(echo "$line" | grep -oP 'for \K(\w+)')
@@ -65,7 +65,7 @@ FAILED_LOGINS=$(journalctl -u ssh --since "5 days ago" | grep "Failed" | while r
 done)
 
 # Compiles all the data into BODY
-BODY="=================================\nLive Server Stats Report\n=================================\nGenerated on: $DATE\n=================================\n\nInternet Status: $INTERNET_UP\n\nInterface Status:\n$WIFI\n$DOCKER\n\nBandwidth on main interface:\n$BANDWIDTH\n\nSystem Temp: $SYSTEM_TEMP\nCPU Temperature: $CPU_TEMP\nSSD Temp1: $SSD1_TEMP\nSSD Temp2: $SSD2_TEMP\n\n$CPU_USAGE\n\n$MEM_USAGE\n\n$DISK_USAGE\n\nTop 5 CPU Processes:\n$TOP5_CPU\n\n Top 5 Memory Processes:\n$TOP5_MEM\n\nServer Uptime:\n$SERVER_UPTIME\n\nLoad Average: $LOAD_AVG\n\nInodes over 10%:\n $INODE\n\nLogged in Users: $USERS\n\nFailed Login Attempts:\n$FAILED_LOGINS\n=========================="
+BODY="=================================\nLive Server Stats Report\n=================================\nGenerated on: $DATE\n=================================\n\nInternet Status: $INTERNET_UP\n\nInterface Status:\n$ETHERNET\n$DOCKER\n\nBandwidth on main interface:\n$BANDWIDTH\n\nSystem Temp: $SYSTEM_TEMP\nCPU Temperature: $CPU_TEMP\nSSD Temp1: $SSD1_TEMP\nSSD Temp2: $SSD2_TEMP\n\n$CPU_USAGE\n\n$MEM_USAGE\n\n$DISK_USAGE\n\nTop 5 CPU Processes:\n$TOP5_CPU\n\n Top 5 Memory Processes:\n$TOP5_MEM\n\nServer Uptime:\n$SERVER_UPTIME\n\nLoad Average: $LOAD_AVG\n\nInodes over 10%:\n $INODE\n\nLogged in Users: $USERS\n\nFailed Login Attempts:\n$FAILED_LOGINS\n=========================="
 
 # Send the email
 echo -e "$BODY" | mutt -F /home/mirage/.muttrc -s "$SUBJECT" -- "$TO_EMAIL"
